e0471117d3a1d7aea6f5da6c63d5b097203e9164
VictoriasCloud <pvi22012004@gmail.com>
2024-03-11 03:48:13 +0700

    keyTranser работает. Получаются списки пользователей и список ключей. -Как сделать post-запрос на отправку кода потдверждения? Как это в бд работать там будет? -после мёрджа нужно раскоммитить token и getOfficeName

--- keyTranser/keyTranser.js
+++ keyTranser/keyTranser.js
@@ -1,16 +1,49 @@
+//раскоммитить псле мёрджа -> var token = localStorage.getItem('token');
+    
+var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiNDlmOWNiODctMDY0ZC00MWIyLTlkMjYtNzQzN2ExNmNjMDNiIiwibmJmIjoxNzEwMTAyMzIwLCJleHAiOjE3MTAxMDU5MjAsImlzcyI6IktleS1Nb25pdG9yaW5nIiwiYXVkIjoiU3R1ZGVudEFuZFRlYWNoZXIifQ.LG1vjxglfm3nd2dBWVyFFINorjHibD36lY0uSmCcqwQ";
+console.log(token) 
+
+
+//раскоммитить после мёрджа -> getOfficeName('https://localhost:7266/api/account/profile', token);
+getKeyList(token);
+
+async function getOfficeName(url, token) {
+    return fetch(url, {
+      method: 'GET',
+      headers: new Headers({
+        "Authorization": `Bearer ${token}`
+      }),
+    })
+      .then(response => response.json())
+      .then(async data => {
+        console.log(data);
+        document.getElementById('user').textContent = data.fullName;
+        if (data.role !== 'DeanOffice'){
+            alert('Ошибка доступа');
+            window.location.href = '/autorization/autorization.html';
+        }
+      })
+      .catch(error => {
+        console.error('Ошибка', error);
+        alert('Ошибка доступа');
+        window.location.href = '/autorization/autorization.html';
+      });
+  }
+
 // Получение списка всех ключей
-async function getKeyList() {
+async function getKeyList(token) {
     const url = 'https://localhost:7266/api/key/GetFullKeyList';
     try {
         const response = await fetch(url, {
             method: 'GET',
-            headers: {
-                'accept': 'application/json'
-            }
+            headers: new Headers({
+                "Authorization": `Bearer ${token}`
+              }),
         });
         const data = await response.json();
         if (data.list) {
             const keys = data.list.map(item => item.cabinetNumber);
+            console.log(keys);
             insertKeysIntoDropdown(keys);
         }
     } catch (error) {
@@ -24,9 +57,9 @@ async function getUserList() {
     try {
         const response = await fetch(url, {
             method: 'GET',
-            headers: {
-                'accept': 'application/json'
-            }
+            headers: new Headers({
+                "Authorization": `Bearer ${token}`
+              }),
         });
         const data = await response.json();
         if (data.name) {
@@ -62,6 +95,7 @@ function insertUsersIntoDropdown(users) {
 
 // Вызов функций для получения списка ключей и пользователей при загрузке страницы
 window.onload = function () {
+    console.log("запуск")
     getKeyList();
     getUserList();
 };
