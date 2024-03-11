8e3b0aa3a4a83e76ff57e32045a5ab8bcf1cf1fa
VictoriasCloud <pvi22012004@gmail.com>
2024-03-11 14:09:49 +0700

    application с get запросами

--- applicationManagment/application.js
+++ applicationManagment/application.js
@@ -1,31 +1,85 @@
-//раскоммитить псле мёрджа -> var token = localStorage.getItem('token');
-    
-var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiNDlmOWNiODctMDY0ZC00MWIyLTlkMjYtNzQzN2ExNmNjMDNiIiwibmJmIjoxNzEwMTAyMzIwLCJleHAiOjE3MTAxMDU5MjAsImlzcyI6IktleS1Nb25pdG9yaW5nIiwiYXVkIjoiU3R1ZGVudEFuZFRlYWNoZXIifQ.LG1vjxglfm3nd2dBWVyFFINorjHibD36lY0uSmCcqwQ";
-console.log(token) 
+document.addEventListener('DOMContentLoaded', function() {
+    // Получение данных с сервера при загрузке страницы
+    getDataFromServer();
 
-//раскоммитить после мёрджа -> getOfficeName('https://localhost:7266/api/account/profile', token);
-getKeyList(token);
+    // Обработчик события для кнопки поиска
+    const searchButton = document.getElementById('searchButton');
+    searchButton.addEventListener('click', function() {
+        searchApplications();
+    });
+});
 
-async function getOfficeName(url, token) {
-    return fetch(url, {
-      method: 'GET',
-      headers: new Headers({
-        "Authorization": `Bearer ${token}`
-      }),
-    })
-      .then(response => response.json())
-      .then(async data => {
-        console.log(data);
-        document.getElementById('user').textContent = data.fullName;
-        if (data.role !== 'DeanOffice'){
-            alert('Ошибка доступа');
-            window.location.href = '/autorization/autorization.html';
+async function getDataFromServer() {
+    const url = 'https://localhost:7266/api/application/applicationsList';
+    const options = {
+        method: 'GET',
+        headers: {
+            'accept': 'application/json',
+            //'status': 1, // Установка параметра status в заголовке запроса
+            'page': 1, // Установка параметра page в заголовке запроса
+            'size': 5 // Установка параметра size в заголовке запроса
         }
-      })
-      .catch(error => {
-        console.error('Ошибка', error);
-        alert('Ошибка доступа');
-        window.location.href = '/autorization/autorization.html';
-      });
-  }
-  
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+        console.log(data);
+        createTable(data.list);
+    } catch (error) {
+        console.error('Ошибка при получении данных с сервера:', error);
+    }
+}
+
+function createTable(applications) {
+    const tableBody = document.querySelector('.table tbody');
+    tableBody.innerHTML = '';
+
+    applications.forEach(function(application, index) {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${index + 1}</td>
+            <td>${formatDateTime(application.date)}</td>
+            <td>${application.status}</td>
+            <td>${application.ownerName}</td>
+            <td>${application.ownerRole}</td>
+            <td>${application.keyNumber} (${application.cabinetNumber})</td>
+            <td></td>
+            <td>${formatDateTime(application.pairStart)}</td>
+            <td>
+                <button class="btn btn-outline-success">Принять</button>
+                <button class="btn btn-outline-danger">Отклонить</button>
+            </td>
+        `;
+        tableBody.appendChild(row);
+    });
+}
+
+function formatDateTime(dateTimeStr) {
+    const dateTime = new Date(dateTimeStr);
+    const formattedDateTime = dateTime.toLocaleString('ru-RU');
+    return formattedDateTime;
+}
+
+async function searchApplications() {
+    const inputName = document.getElementById('inputName').value;
+    const url = `https://localhost:7266/api/application/applicationsList?partOfName=${inputName}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'accept': 'application/json',
+            //'status': 1, // Установка параметра status в заголовке запроса
+            'page': 2, // Установка параметра page в заголовке запроса
+            'size': 8 // Установка параметра size в заголовке запроса
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+        console.log(data);
+        createTable(data.list);
+    } catch (error) {
+        console.error('Ошибка при поиске заявок:', error);
+    }
+}
