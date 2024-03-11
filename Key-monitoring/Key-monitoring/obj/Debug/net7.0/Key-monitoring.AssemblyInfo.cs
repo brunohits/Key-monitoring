4a4e4df25828a482852e2385762d6ca508168d33
chebupelek <mistiquelico@gmail.com>
2024-03-11 13:35:20 +0700

    ну чисто для вида

--- Key-monitoring/Key-monitoring/obj/Debug/net7.0/Key-monitoring.AssemblyInfo.cs
+++ Key-monitoring/Key-monitoring/obj/Debug/net7.0/Key-monitoring.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Key-monitoring")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf9c5bcc07546162cdae84df4db9103a35f98fe6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbfc0be16badcd26ca89da150d42c034535c7771")]
 [assembly: System.Reflection.AssemblyProductAttribute("Key-monitoring")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Key-monitoring")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
